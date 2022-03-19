11c7ee971834bd50b16e3282df47b63a63aa5084
Jakub Kułak <jakub.kulak@gmail.com>
2022-03-19 11:12:24 +0100

    ♻️ Move components to Components and use only j.sx for jsx

--- www/app/src/index.js
+++ www/app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./pure.css";
 import "./index.css";
-import App from "./App";
+import App from "./Components/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
