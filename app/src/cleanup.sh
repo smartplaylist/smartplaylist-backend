e6cd16709195ca6d2e186a3b8cc7ce1419b6aace
Jakub Kułak <jakub.kulak@gmail.com>
2022-05-04 23:49:54 +0200

    Don't cache tracks requests locally

--- app/src/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker run -ti -v $(pwd)/db:/db --rm --network spotify-grabtrack_default alembic-image alembic downgrade base
-docker run -ti -v $(pwd)/db:/db --rm --network spotify-grabtrack_default alembic-image alembic upgrade
-docker run -ti --network spotify-grabtrack_default -v $(pwd)/app/src:/app --env-file .env --rm spotify-grabtrack_app pipenv run python delete_queues.py
--- app/src/get_track_details.py
+++ app/src/get_track_details.py
@@ -8,7 +8,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import imports.broker as broker
 import imports.db as db
 from imports.logging import get_logger
-import imports.requests
 
 READING_QUEUE_NAME = "tracks"
 PREFETCH_COUNT = 50
