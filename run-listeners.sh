ac86cda437717c3de432c6b2e9dfed04215e335b
Jakub Kułak <jakub.kulak@gmail.com>
2022-08-17 00:03:42 +0200

    Use bash script for running multiple listeners

---
+++ run-listeners.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+(trap 'kill 0' SIGINT;
+rm ./app/src/.cache
+docker run -ti --network spotify-grabtrack_default -v $(pwd)/app/src:/app --env-file .env --rm spotify-grabtrack_app pipenv run python get_albums.py &
+docker run -ti --network spotify-grabtrack_default -v $(pwd)/app/src:/app --env-file .env --rm spotify-grabtrack_app pipenv run python get_albums.py &
+docker run -ti --network spotify-grabtrack_default -v $(pwd)/app/src:/app --env-file .env --rm spotify-grabtrack_app pipenv run python get_albums.py &
+docker run -ti --network spotify-grabtrack_default -v $(pwd)/app/src:/app --env-file .env --rm spotify-grabtrack_app pipenv run python get_album_details.py &
+docker run -ti --network spotify-grabtrack_default -v $(pwd)/app/src:/app --env-file .env --rm spotify-grabtrack_app pipenv run python get_album_details.py &
+docker run -ti --network spotify-grabtrack_default -v $(pwd)/app/src:/app --env-file .env --rm spotify-grabtrack_app pipenv run python get_album_details.py &
+docker run -ti --network spotify-grabtrack_default -v $(pwd)/app/src:/app --env-file .env --rm spotify-grabtrack_app pipenv run python get_track_details.py
+)
